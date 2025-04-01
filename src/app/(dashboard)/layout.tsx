9ba390a96948132aab50596a91aaac846b7fb6b7
MarlonDev <marlonpondavilla@dev.com>
2025-04-01 20:09:13 +0800

    update layout colors; enhance ProfilePage styling; improve post input design and button colors

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -6,7 +6,7 @@ export default function HomeLayout({
   children: React.ReactNode
 }){
   return(
-    <div className="bg-gray-300">
+    <div className="bg-gray-800">
       <Navbar/>
       {children}
     </div>
--- src/app/(dashboard)/profile/profile-page.tsx
+++ src/app/(dashboard)/profile/profile-page.tsx
@@ -17,36 +17,36 @@ const ProfilePageClient = () => {
             className="rounded-full border-2 border-gray-300 dark:border-gray-700" 
             alt=""
           />
-          <span className="bottom-1 right-3 absolute w-4.5 h-4.5 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full" title="Active now"></span>
+          <span className="bottom-1 right-3 absolute w-4 h-4 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full" title="Active now"></span>
         </div>  
-        <div className="flex flex-col items-center justify-center text-base font-semibold text-gray-900 dark:text-white space-y-2">
+        <div className="flex flex-col items-center justify-center text-base font-semibold text-white dark:text-white space-y-2">
           <p>{userData?.displayName || "No name"}</p>
           <p className="bg-purple-100 text-purple-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-purple-900 dark:text-purple-300 text-center">FullStack</p>
         </div>
-        <div className="create-post-container flex-col items-center justify-center gap-2 border-2 text-md p-4    font-light mt-4">
-          <div className="post-input">
+        <div className="create-post-container flex-col items-center justify-center gap-2 border-2 text-md p-4 font-light mt-4 w-[38rem] rounded-b-md">
+          <div className="post-input flex justify-center space-x-2">
             <input
               type="text"
               placeholder="Write or upload dev related stuff to create a post."
-              className="border-2 border-gray-300 rounded-full p-3 w-[24rem] font-light text-md bg-gray-100"  
+              className="border-2 border-gray-300 rounded-full p-3 w-[26rem] font-light text-md bg-gray-9 text-gray-200"  
             />
-            <button className="bg-green-500 py-3 w-[6rem] rounded-full hover:bg-green-400 cursor-pointer font-semibold text-gray-800">Post</button>
+            <button className="bg-blue-500 py-3 w-[6rem] rounded-full hover:bg-blue-400 cursor-pointer font-semibold text-gray-800">Post</button>
           </div>
           <div className="post-options flex items-center justify-center space-x-8 mt-4">
             <span className="photo-upload border-2 p-2 cursor-pointer" title="Upload a photo">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-white">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
               </svg>
             </span>
 
             <span className="code-upload border-2 p-2 cursor-pointer" title="Upload a code snippet">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-white">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
               </svg>
             </span>
 
             <span className="emoji-upload border-2 p-2 cursor-pointer" title="Upload feelings">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-white">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z" />
               </svg>
               </span>
